518cb39f6bca45be4eb867229eda72d090756f49
Michael Samper <msamper@growthbook.io>
2025-05-09 11:05:06 -0400

    add get_stale_safe_rollouts tool

--- src/tools/features.ts
+++ src/tools/features.ts
@@ -187,6 +187,72 @@ export function registerFeatureTools({
     }
   );
 
+  server.tool(
+    "get_stale_safe_rollouts",
+    "Fetches all complete safe rollouts (rolled-back or released) from the GrowthBook API",
+    {
+      limit: z.number().optional().default(10),
+      offset: z.number().optional().default(0),
+      project: z.string().optional(),
+    },
+    async ({ limit, offset, project }) => {
+      try {
+        const queryParams = new URLSearchParams({
+          limit: limit?.toString(),
+          offset: offset?.toString(),
+        });
+
+        if (project) queryParams.append("project", project);
+
+        const res = await fetch(
+          `${baseApiUrl}/features?${queryParams.toString()}`,
+          {
+            headers: {
+              Authorization: `Bearer ${apiKey}`,
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        await handleResNotOk(res);
+
+        const data = await res.json();
+
+        const filteredSafeRollouts = data.features.filter((feature: any) => {
+          const envs: Record<string, any> = feature.environments;
+          if (!envs) return false;
+          return Object.values(envs).some((env: any) => {
+            const rules = env.rules;
+            if (!rules) return false;
+            return rules.some((rule: any) => {
+              return (
+                rule.type === "safe-rollout" &&
+                (rule.status === "rolled-back" || rule.status === "released")
+              );
+            });
+          });
+        });
+
+        const text = `
+      ${JSON.stringify(filteredSafeRollouts, null, 2)}
+  
+      Share information about the rolled-back or released safe rollout rules with the user. Safe Rollout rules are stored under 
+      environmentSettings, keyed by environment and are within the rules array with a type of "safe-rollout". Ask the user if they
+      would like to remove references to the feature associated with the rolled-back or released safe rollout rules and if they do,
+      remove the references and replace the values with controlValue if the safe rollout rule is rolled-back or with the 
+      variationValue if the safe rollout is released. In addition to the current file, you may need to update other files in the codebase.
+      `;
+
+        return {
+          content: [{ type: "text", text }],
+        };
+      } catch (error) {
+        console.error("Error fetching stale safe rollouts:", error);
+        throw error;
+      }
+    }
+  );
+
   // server.tool(
   //   "generate_flag_types",
   //   "Generate types for a feature flags",
